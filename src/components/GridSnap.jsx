54ee4b30bfa1a99a9a7c6ccf4cce0ea7fe981d51
Greg Xu <gouwa5700@gmail.com>
2022-05-17 13:48:57 +0800

    save

--- src/components/GridSnap.jsx
+++ src/components/GridSnap.jsx
@@ -2,116 +2,68 @@ import interact from "interactjs";
 import React from "react";
 
 export default function GridSnap() {
-    var x = 0; var y = 0;
-
-    interact("#grid-snap1")
-        .draggable({
-            modifiers: [
-                interact.modifiers.snap({
-                    targets: [
-                        interact.snappers.grid({
-                            x: 360,
-                            y: 48,
-                            limits: {
-                                top: 0,
-                                left: 0,
-                                bottom: 48,
-                                right: 360*3
-                            }
-                        })
-                    ],
-                    range: Infinity,
-                    relativePoints: [{ x: 0, y: 0 }]
-                }),
-                interact.modifiers.restrict({
-                    elementRect: { top: 0, left: 0, bottom: 0, right: 0 },
-                    restriction: 'parent',
-                })
-            ],
-            inertia: true
+  interact(".grid-snap")
+    .draggable({
+      modifiers: [
+        interact.modifiers.snap({
+          targets: [
+            interact.snappers.grid({
+              x: 100,
+              y: 50
+            })
+          ],
+          range: Infinity,
+          relativePoints: [{ x: 0, y: 0 }]
+        }),
+        interact.modifiers.restrict({
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+          restriction: 'parent',
+          endOnly: true
         })
-        .on('dragmove', function (event) {
-            x += event.dx
-            y += event.dy
+      ],
+      autoScroll: true,
+      listeners: {
+        move(event) {
+          let target = event.target;
+          let x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+          target.style.transform = 'translateX(' + x + 'px)';
+          target.setAttribute('data-x', x);
+        }
+      }
+    });
 
-            event.target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-        });
-    interact("#grid-snap2")
-        .draggable({
-            modifiers: [
-                interact.modifiers.snap({
-                    targets: [
-                        interact.snappers.grid({
-                            x: 360,
-                            y: 48,
-                            limits: {
-                                top: 0,
-                                left: 0,
-                                bottom: 48,
-                                right: 360*3
-                            }
-                        })
-                    ],
-                    range: Infinity,
-                    relativePoints: [{ x: 0, y: 0 }]
-                }),
-                interact.modifiers.restrict({
-                    elementRect: { top: 0, left: 0, bottom: 0, right: 0 },
-                    restriction: 'parent',
-                })
-            ],
-            inertia: true
-        })
-        .on('dragmove', function (event) {
-            x += event.dx
-            y += event.dy
-
-            event.target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-        });
-    interact("#grid-snap3")
-        .draggable({
-            modifiers: [
-                interact.modifiers.snap({
-                    targets: [
-                        interact.snappers.grid({
-                            x: 360,
-                            y: 48,
-                            limits: {
-                                top: 0,
-                                left: 0,
-                                bottom: 48,
-                                right: 360*3
-                            }
-                        })
-                    ],
-                    range: Infinity,
-                    relativePoints: [{ x: 0, y: 0 }]
-                }),
-                interact.modifiers.restrict({
-                    elementRect: { top: 0, left: 0, bottom: 0, right: 0 },
-                    restriction: 'parent',
-                })
-            ],
-            inertia: true
-        })
-        .on('dragmove', function (event) {
-            x += event.dx
-            y += event.dy
-
-            event.target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-        });
-
-    return (
-        <>
-            <div id="grid-snap1">
-                Dragging is constrained to the corners of a grid
-            </div>
-            <div id="grid-snap2">
-                Dragging is constrained to the corners of a grid
-            </div>
-            <div id="grid-snap3">
-                Dragging is constrained to the corners of a grid
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="grid-line">
+        <div className="grid-snap">
+          Drag
+        </div>
+      </div>
+      <div className="grid-line">
+        <div className="grid-snap">
+          Drag
+        </div>
+      </div>
+      <div className="grid-line">
+        <div className="grid-snap">
+          Drag
+        </div>
+      </div>
+      <div className="grid-line">
+        <div className="grid-snap">
+          Drag
+        </div>
+      </div>
+      <div className="grid-line">
+        <div className="grid-snap">
+          Drag
+        </div>
+      </div>
+      <div className="grid-line">
+        <div className="grid-snap">
+          Drag
+        </div>
+      </div>
+    </>
+  );
 }
