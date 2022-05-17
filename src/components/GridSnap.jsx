ae65e422ba2513fe01a240e5f241593ee64e52bc
Greg Xu <gouwa5700@gmail.com>
2022-05-17 23:09:14 +0800

    12 column snap grids

--- src/components/GridSnap.jsx
+++ src/components/GridSnap.jsx
@@ -1,66 +1,51 @@
 import interact from "interactjs";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function GridSnap() {
-  interact(".grid-snap")
-    .draggable({
-      modifiers: [
-        interact.modifiers.snap({
-          targets: [
-            interact.snappers.grid({
-              x: 100,
-              y: 50
-            })
-          ],
-          range: Infinity,
-          relativePoints: [{ x: 0, y: 0 }]
-        }),
-        interact.modifiers.restrict({
-          elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
-          restriction: 'parent',
-          endOnly: true
-        })
-      ],
-      autoScroll: true,
-      listeners: {
-        move(event) {
-          let target = event.target;
-          let x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-          target.style.transform = 'translateX(' + x + 'px)';
-          target.setAttribute('data-x', x);
+  const gridSnapRef = React.createRef();
+
+  useEffect(() => {
+    const gridSnap = interact(gridSnapRef.current);
+
+    gridSnap
+      .draggable({
+        modifiers: [
+          interact.modifiers.snap({
+            targets: [
+              interact.snappers.grid({
+                x: 100,
+                y: 50
+              })
+            ],
+            range: Infinity,
+            relativePoints: [{ x: 0, y: 0 }]
+          }),
+          interact.modifiers.restrict({
+            elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+            restriction: 'parent',
+            endOnly: true
+          })
+        ],
+        autoScroll: true,
+        listeners: {
+          move(event) {
+            let target = event.target;
+            let x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+            target.style.transform = 'translateX(' + x + 'px)';
+            target.setAttribute('data-x', x);
+          }
         }
-      }
-    });
+      });
+
+    return function cleanup() {
+      gridSnap.unset();
+    };
+  }, []);
 
   return (
     <>
       <div className="grid-line">
-        <div className="grid-snap">
-          Drag
-        </div>
-      </div>
-      <div className="grid-line">
-        <div className="grid-snap">
-          Drag
-        </div>
-      </div>
-      <div className="grid-line">
-        <div className="grid-snap">
-          Drag
-        </div>
-      </div>
-      <div className="grid-line">
-        <div className="grid-snap">
-          Drag
-        </div>
-      </div>
-      <div className="grid-line">
-        <div className="grid-snap">
-          Drag
-        </div>
-      </div>
-      <div className="grid-line">
-        <div className="grid-snap">
+        <div ref={gridSnapRef} className="grid-snap">
           Drag
         </div>
       </div>
--- src/components/TimelineYear.jsx
+++ src/components/TimelineYear.jsx
@@ -1,37 +1,15 @@
 import React from "react";
-import interact from 'interactjs'
 import GridSnap from "./GridSnap";
 
 export default function TimelineYear(props) {
-  const slider = interact('.slider');    // target elements with the "slider" class
-
-  slider
-    // Step 2
-    .draggable({                        // make the element fire drag events
-      origin: 'self',                   // (0, 0) will be the element's top-left
-      inertia: true,                    // start inertial movement if thrown
-      modifiers: [
-        interact.modifiers.restrict({
-          restriction: 'self'           // keep the drag coords within the element
-        })
-      ],
-      // Step 3
-      listeners: {
-        move(event) {                  // call this listener on every dragmove
-          const sliderWidth = interact.getElementRect(event.target).width
-          const value = event.pageX / sliderWidth
-
-          event.target.style.paddingLeft = (value * 100) + '%'
-          event.target.setAttribute('data-value', value.toFixed(2))
-        }
-      }
-    });
-
   return (
     <>
-      <div className="grid-line">
-        <GridSnap />
-      </div>
+      <GridSnap />
+      <GridSnap />
+      <GridSnap />
+      <GridSnap />
+      <GridSnap />
+      <GridSnap />
     </>
   );
 }
