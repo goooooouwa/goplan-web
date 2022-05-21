b89ab2666caa86d59c3cf1acd5811415ab471230
Greg Xu <gouwa5700@gmail.com>
2022-05-21 10:19:13 +0800

    clean up grid snap code

--- src/components/GridSnap.jsx
+++ src/components/GridSnap.jsx
@@ -1,32 +1,23 @@
 import { Rnd } from "react-rnd";
-import React, { useEffect, useState, CSSProperties } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const gridSnapStyle = {
   width: "100px",
   height: "50px",
   backgroundColor: "#29e",
   color: "#fff",
-  borderStyle: "solid",
-  borderColor: "black",
-  fontSize: "1.2em",
 }
 
 const gridLineStyle = {
   width: "1200px",
   height: "50px",
-  borderStyle: "solid",
-  borderColor: "gray",
 }
 
 export default function GridSnap(props) {
   const [width, setWidth] = useState(100);
-  const [height, setHeight] = useState(50);
+  const height = 50;
   const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-
-  function handleChange(event) {
-    setX(event.target.value);
-  }
+  const y = 0;
 
   function handleDragStop(event, data) {
     const columnDelta = Math.floor((data.x - x) / width);
@@ -40,32 +31,23 @@ export default function GridSnap(props) {
 
   return (
     <>
-      <label>
-        Times
-        <input
-          type="number"
-          name="repeatTimes"
-          value={x}
-          onChange={handleChange}
-        />
-      </label>
       <div style={gridLineStyle}>
-      <Rnd
-        style={gridSnapStyle}
-        size={{ width: width,  height: height }}
-        position={{ x: x, y: y }}
-        onResize={(e, direction, ref, delta, position) => {
-          setWidth(ref.offsetWidth);
-          setX(position.x);
-        }}
-        resizeGrid={[100, 30]}
-        dragGrid={[100, 30]}
-        dragAxis={'x'}
-        bounds={'parent'}
-        onDragStop={handleDragStop}
-      >
-        {x} + {y}
-      </Rnd>
+        <Rnd
+          style={gridSnapStyle}
+          size={{ width: width, height: height }}
+          position={{ x: x, y: y }}
+          onResize={(e, direction, ref, delta, position) => {
+            setWidth(ref.offsetWidth);
+            setX(position.x);
+          }}
+          resizeGrid={[100, 30]}
+          dragGrid={[100, 30]}
+          dragAxis={'x'}
+          bounds={'parent'}
+          onDragStop={handleDragStop}
+        >
+          {props.title}
+        </Rnd>
       </div>
     </>
   );
