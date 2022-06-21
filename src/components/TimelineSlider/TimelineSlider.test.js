c852cef8bac8b91d91e4ba741124a2827a96729e
Shunfa Xu <gouwa5700@gmail.com>
2022-06-21 12:15:47 +0800

    no test for slider since don't know how to drag

--- src/components/TimelineSlider/TimelineSlider.test.js
+++ src/components/TimelineSlider/TimelineSlider.test.js
@@ -3,26 +3,43 @@ import renderer from 'react-test-renderer';
 import TimelineSlider from './TimelineSlider';
 
 const marks = [{
-  value: 1,
+  value: 0,
   label: "Week 1"
 }, {
-  value: 2,
+  value: 1,
   label: "Week 2"
+}, {
+  value: 2,
+  label: "Week 3"
+}, {
+  value: 3,
+  label: "Week 4"
+}, {
+  value: 4,
+  label: "Week 5"
 }];
 
 it('should show a timeline slider', () => {
   const component = renderer.create(
-    <TimelineSlider
-      marks={marks}
-      rangeMin={0}
-      rangeMax={4}
-      rangeStart={1}
-      rangeEnd={3}
-      disableRangeStart={true}
-      disableRangeEnd={false}
-      handleChangeCommited={() => { }}
-    />
+     <TimelineSlider
+       marks={marks}
+       rangeMin={0}
+       rangeMax={4}
+       rangeStart={1}
+       rangeEnd={3}
+       disableRangeStart={false}
+       disableRangeEnd={false}
+       handleChangeCommited={() => {}}
+     />
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('should disable rangeStart if disableRangeStart is true', () => {
+  // left empty since I don't know how to simulate drag event with react-testing-library
+});
+
+it('should call #handleChangeCommited when new rangeStart or rangeEnd selected', async () => {
+  // left empty since I don't know how to simulate drag event with react-testing-library
 });
