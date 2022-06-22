ef925018cf642f22882b4ef138b2f86ae4797388
Shunfa Xu <gouwa5700@gmail.com>
2022-06-22 23:19:11 +0800

    add some empty tests for later

---
+++ src/views/timeline/TimelineYearContainer/TimelineYearContainer.test.js
@@ -0,0 +1,20 @@
+it('should show a Timeline Year View with action bar', () => {
+});
+
+it('should call #handleTodoChange if any todo changed', () => {
+});
+
+it('should refresh todos if todosUrl or any todo changed', () => {
+});
+
+it('should call #handleMonthChange if start or end month changed', () => {
+});
+
+it('should call #handleTodayClick if today button clicked', () => {
+});
+
+it('should call #handlePreviousYearClick if previous year button clicked', () => {
+});
+
+it('should call #handleNextYearClick if next year button clicked', () => {
+});
