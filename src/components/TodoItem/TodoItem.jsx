1cf948e68a6a6692ecdd6f940fa29e881f1e54cf
Shunfa Xu <gouwa5700@gmail.com>
2022-06-25 23:32:04 +0800

    add secondaryAction to expand todo

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,4 +1,4 @@
-import { Checkbox, Collapse, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { Checkbox, Collapse, IconButton, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import React from "react";
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
@@ -14,7 +14,22 @@ export default function TodoItem(props) {
 
   return (
     <>
-      <ListItem component="div" disablePadding>
+      <ListItem
+        component="div"
+        disablePadding
+        secondaryAction={
+          <>
+            {
+              props.todo.dependents.length > 0 &&
+              <IconButton edge="end" aria-label="expand" onClick={handleClick}>
+                  {
+                    open ? <ExpandLess /> : <ExpandMore />
+                  }
+              </IconButton>
+            }
+          </>
+        }
+      >
         <Checkbox
           checked={props.todo.status}
           onChange={(event) => { props.handleTodoChange(event, props.todo) }}
@@ -22,47 +37,12 @@ export default function TodoItem(props) {
         />
         <ListItemButton component={Link} to={`/projects/${props.todo.projectId}/todos/${props.todo.id}`}>
           <ListItemText
-            primary={
-              props.todo.name
-            }
+            primary={props.todo.name}
             sx={{
               textDecoration: props.todo.status ? 'line-through' : 'none',
               color: props.todo.status ? 'text.secondary' : 'text.primary',
             }} />
         </ListItemButton>
-        {props.todo.dependents.length > 0 &&
-          <>
-            {
-              open ? <ExpandLess onClick={handleClick} /> : <ExpandMore onClick={handleClick} />
-            }
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <List component="div" disablePadding>
-                {
-                  props.todo.dependents
-                    .map((todo, index) => (
-                      <ListItem key={index}>
-                        <Checkbox
-                          checked={props.todo.status}
-                          onChange={(event) => { props.handleTodoChange(event, todo) }}
-                          inputProps={{ 'aria-label': 'controlled' }}
-                        />
-                        <ListItemButton component={Link} to={`/projects/${todo.projectId}/todos/${todo.id}`}>
-                          <ListItemText
-                            primary={
-                              todo.name
-                            }
-                            sx={{
-                              textDecoration: props.todo.status ? 'line-through' : 'none',
-                              color: props.todo.status ? 'text.secondary' : 'text.primary',
-                            }} />
-                        </ListItemButton>
-                      </ListItem>
-                    ))
-                }
-              </List>
-            </Collapse>
-          </>
-        }
       </ListItem>
     </>
   );
