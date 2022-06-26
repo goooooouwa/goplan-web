e56d68e0c86b41897140ba827c365cfb554ce6bc
Shunfa Xu <gouwa5700@gmail.com>
2022-06-26 14:04:11 +0800

    flattenTodosAndDependents

--- src/utils/todoTraversal.js
+++ src/utils/todoTraversal.js
@@ -10,6 +10,16 @@ const markDirtyTodosAndDependents = (todos, updatedTodoId, { startDate, endDate 
     return todo;
   });
 };
+  
+const flattenTodosAndDependents = (todos) => {
+  return todos.map((todo) => {
+    if (todo.dependents.length > 0) {
+      return [todo, flattenTodosAndDependents(todo.dependents)].flat();
+    } else {
+      return todo;
+    }
+  });
+};
 
 const todoTraversal = {
   markDirtyTodosAndDependents
