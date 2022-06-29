9153f046ac2a6cf92eb4cef938d1eb28e84db35d
Shunfa Xu <gouwa5700@gmail.com>
2022-06-29 13:12:21 +0800

    add logo

--- src/views/app/LandingPage.jsx
+++ src/views/app/LandingPage.jsx
@@ -1,7 +1,8 @@
-import { Button, Container, Grid, Link, Stack } from "@mui/material";
+import { Button, Container, Grid, Link, Stack, Typography } from "@mui/material";
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "hooks/useAuth";
+import TaskAlt from "@mui/icons-material/TaskAlt";
 
 export default function LandingPage() {
   const { getAccessToken, signInPage, handleOfflineMode } = useAuth();
@@ -14,7 +15,12 @@ export default function LandingPage() {
       <Container sx={{ mt: 2 }}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <h1>GoPlan</h1>
+            <Stack direction="row" alignItems="center">
+              <TaskAlt fontSize="large" sx={{mr: 0 }} color="success" />
+              <Typography variant="h3" color="text.primary" >
+                GoPlan
+              </Typography>
+            </Stack>
             <h2>Where todos meets a timeline</h2>
             <p>GoPlan is an app to help you achieve your goals, by turning your plan into actionable tasks that can be easily managed over time with a timeline overview.</p>
           </Grid>
