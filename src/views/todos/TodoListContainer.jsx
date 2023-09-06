2d0d05112f8a545ef21472d7fbfe87df2901b08f
Shunfa Xu <gouwa5700@gmail.com>
2023-09-06 21:43:21 +0800

    refactor: use state from parent route

--- src/views/todos/TodoListContainer.jsx
+++ src/views/todos/TodoListContainer.jsx
@@ -1,76 +1,22 @@
-import httpService from "services/httpService";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useParams, useOutletContext } from "react-router-dom";
 import TodoListItem from "components/TodoListItem";
 import { Container, Grid, Stack, Typography } from "@mui/material";
-import { useParams } from "react-router-dom";
 import TodoActionGroup from "components/TodoActionGroup";
-import { useAPIError } from "hooks/useAPIError";
-import todoTraversal from "utils/todoTraversal";
 import { useTranslation } from 'react-i18next';
 
 export default function TodoListContainer() {
   const { t, i18n } = useTranslation();
   const params = useParams();
-  const url = params.projectId !== undefined ? `/todos.json?project_id=${params.projectId}` : '/todos.json';
-  const [todos, setTodos] = useState([]);
-  const { addError } = useAPIError();
+  const todosUrl = params.projectId !== undefined ? `/todos.json?project_id=${params.projectId}` : '/todos.json';
+  const [todos, _, updateTodoStatus, loadChildren, reloadTodos] = useOutletContext();
 
   useEffect(() => {
-    reloadTodos();
-  }, [params.projectId]);
-
-  const reloadTodos = () => {
-    httpService.get(url)
-      .then((response) => {
-        setTodos(response.data);
-      })
-      .catch(function (error) {
-        // handle error
-        addError(error.response.data, error.response.status);
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
-  };
+    reloadTodos(todosUrl);
+  }, [todosUrl]);
 
   const handleTodoChange = (event, todo) => {
-    const todoData = {
-      status: event.target.checked,
-    };
-
-    httpService.put(`/todos/${todo.id}.json`, todoData)
-      .then((response) => {
-        const updatedTodo = response.data;
-        setTodos((todos) => {
-          return todoTraversal.updateTodosAndChildren(todos, updatedTodo);
-        });
-      })
-      .catch(function (error) {
-        addError(error.response.data, error.response.status);
-        console.log(error);
-      });
-  };
-
-  const loadChildren = (todo) => {
-    if (!(todo.numberOfChildren > 0 && todo.children.length === 0)) {
-      return;
-    }
-
-    httpService.get(`/todos/${todo.id}/children.json`)
-      .then((response) => {
-        const updatedTodo = {
-          ...todo,
-          children: response.data,
-        }
-        setTodos((todos) => {
-          return todoTraversal.updateTodosAndChildren(todos, updatedTodo);
-        });
-      })
-      .catch(function (error) {
-        addError(error.response.data, error.response.status);
-        console.log(error);
-      });
+    updateTodoStatus(event, todo, () => {});
   };
 
   return (
