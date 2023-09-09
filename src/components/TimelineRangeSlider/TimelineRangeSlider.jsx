64daa38d61779244370877ceb9bbdb3f31b7d9a7
Shunfa Xu <gouwa5700@gmail.com>
2023-09-09 19:26:44 +0800

    disable out-of-range tasks in timeline

--- src/components/TimelineRangeSlider/TimelineRangeSlider.jsx
+++ src/components/TimelineRangeSlider/TimelineRangeSlider.jsx
@@ -54,6 +54,7 @@ export default function TimelineRangeSlider(props) {
       <Slider
         valueLabelFormat={valueLabelFormat}
         value={value}
+        disabled={props.disableRangeStart && props.disableRangeEnd && (value[0] === value[1])}
         onChange={(loading || isSmallScreen) ? () => { } : handleChange}
         onChangeCommitted={(loading || isSmallScreen) ? () => { } : handleChangeCommitted}
         getAriaValueText={valuetext}
